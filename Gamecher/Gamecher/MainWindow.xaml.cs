6ae01f8f97342338806a4c65b4e5a14aa1bb9232
Aitor Mecinas <aiyoju_steamgifts@hotmail.es>
2018-05-11 01:15:33 +0200

    Almost finished main window look. Almost finished customizing the top bar. Fixed a lot of problems with window sizes and maximizing, etc...

--- Gamecher/Gamecher/MainWindow.xaml.cs
+++ Gamecher/Gamecher/MainWindow.xaml.cs
@@ -18,11 +18,83 @@ namespace Gamecher
     /// <summary>
     /// Lógica de interacción para MainWindow.xaml
     /// </summary>
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            int i = 0;
+            TextBlock lol;
+            while (i < 90)
+            {
+                lol = new TextBlock
+                {
+                    Text = "Pene"
+                };
+                wrapMahepanel.Children.Add(lol);
+                i++;
+            }
         }
+
+        /// <summary>
+        /// TitleBar_MouseDown - Drag if single-click, resize if double-click
+        /// </summary>
+        private void WindowTopBarClicked(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                if (e.ClickCount == 2)
+                {
+                    AdjustWindowSize();
+                }
+                else
+                {
+                    Application.Current.MainWindow.DragMove();
+                }
+        }
+
+        /// <summary>
+        /// CloseButton_Clicked
+        /// </summary>
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        /// <summary>
+        /// MaximizedButton_Clicked
+        /// </summary>
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            AdjustWindowSize();
+        }
+
+        /// <summary>
+        /// Minimized Button_Clicked
+        /// </summary>
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        /// <summary>
+        /// Adjusts the WindowSize to correct parameters when Maximize button is clicked
+        /// </summary>
+        private void AdjustWindowSize()
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                this.ResizeMode = ResizeMode.CanResizeWithGrip;
+                this.WindowState = WindowState.Normal;
+            }
+            else
+            {
+                this.MaxHeight = SystemParameters.WorkArea.Height;
+                this.ResizeMode = ResizeMode.NoResize;
+                this.WindowState = WindowState.Maximized;
+            }
+
+        }
+
     }
 }
