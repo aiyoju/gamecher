4351a0dbfd900a619d7eb004a984f57ba8314f7c
Aitor Mecinas <aiyoju_steamgifts@hotmail.es>
2018-05-21 18:13:16 +0200

    Trying to get the game search function working.

--- Gamecher/Gamecher/MainWindow.xaml.cs
+++ Gamecher/Gamecher/MainWindow.xaml.cs
@@ -1,17 +1,12 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Permissions;
+using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Gamecher
 {
@@ -24,17 +19,6 @@ namespace Gamecher
         public MainWindow()
         {
             InitializeComponent();
-            /*int i = 0;
-            TextBlock lol;
-            while (i < 90)
-            {
-                lol = new TextBlock
-                {
-                    Text = "PenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePenePene"
-                };
-                wrapMahepanel.Children.Add(lol);
-                i++;
-            }*/
         }
 
         /// <summary>
@@ -51,6 +35,98 @@ namespace Gamecher
                 {
                     Application.Current.MainWindow.DragMove();
                 }
+
+            DirectoryInfo dir_info = new DirectoryInfo("C:\\");
+            List<string> dir_list = new List<string>();
+            SearchDirectory(dir_info, dir_list);
+
+            foreach (string i in dir_list)
+            {
+                if (i.Contains("coprocmanager"))
+                {
+                    string awidnawod = new Regex(@"\bcoprocmanager\b").Split(i)[0] + "coprocmanager";
+                    Console.WriteLine(awidnawod);
+                    break;
+                }
+            };
+
+            dir_info = new DirectoryInfo("D:\\");
+            dir_list = new List<string>();
+            SearchDirectory(dir_info, dir_list);
+
+            foreach (string i in dir_list)
+            {
+                if (i.Contains("steamapps"))
+                {
+                    string awidnawod = new Regex(@"\bsteamapps\b").Split(i)[0] + "steamapps";
+                    Console.WriteLine(awidnawod);
+                    break;
+                }
+            };
+
+            string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey(registry_key))
+            {
+                foreach (string subkey_name in key.GetSubKeyNames())
+                {
+                    using (RegistryKey subkey = key.OpenSubKey(subkey_name))
+                    {
+                        if (subkey.GetValue("DisplayName") != null && subkey.GetValue("InstallLocation") != null)
+                        {
+                            if (!subkey.GetValue("InstallLocation").ToString().Equals(""))
+                            {
+                                Console.WriteLine("DisplayName: " + subkey.GetValue("DisplayName"));
+                                Console.WriteLine("InstallLocation: " + subkey.GetValue("InstallLocation"));
+                                List<String> exeList = GetFiles(subkey.GetValue("InstallLocation").ToString(), ".exe", SearchOption.AllDirectories).Cast<String>().ToList();
+                                exeList.ForEach(i => Console.WriteLine(i));
+
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+            }
+
+            //List<String> prueba = Search("D:/", ".txt", SearchOption.AllDirectories);
+            //prueba.ForEach(i => Console.WriteLine(i));
+            
+
+        }
+
+        public static IEnumerable<string> GetFiles(string path,
+                       string searchPatternExpression = "",
+                       SearchOption searchOption = SearchOption.AllDirectories)
+        {
+            Regex reSearchPattern = new Regex(searchPatternExpression, RegexOptions.IgnoreCase);
+            return Directory.EnumerateFiles(path, "*", searchOption)
+                            .Where(file =>
+                                     reSearchPattern.IsMatch(Path.GetExtension(file)));
+        }
+
+        private void SearchDirectory(DirectoryInfo dir_info, List<string> dir_list)
+        {
+            try
+            {
+                foreach (DirectoryInfo subdir_info in dir_info.GetDirectories())
+                {
+                    
+                    SearchDirectory(subdir_info, dir_list);
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            try
+            {
+                foreach (DirectoryInfo subsubdir_info in dir_info.GetDirectories())
+                {
+                    dir_list.Add(dir_info.FullName);
+                }
+            }
+            catch
+            {
+            }
         }
 
         /// <summary>
@@ -100,6 +176,13 @@ namespace Gamecher
         {
             var userSettings = new UserSettings();
             userSettings.ShowDialog();
+        }
+
+        private void AddGameClicked(object sender, MouseButtonEventArgs e)
+        {
+            GameCard gC = new GameCard();
+            gC.PlayButton.MouseUp += AddGameClicked;
+            wrapMahepanel.Children.Add(gC);
         }
     }
 }
